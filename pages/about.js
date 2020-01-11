b4e0bf6fb82a114b680edd71719d93e6ee935113
Master Ace <gapotanimpuno@gmail.com>
2020-01-11 17:50:35 +0800

    Blog content

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,29 @@
-export default = () => (
-    <div>
-        <h1>About
-        </h1>
-        <p> A JavaScript Programmer</p>
-        <img src="/static/javascript-logo.png" alt="JavaScript"></img>
-    </div>
-);
+import Layout from '../components/Layout';
+import fetch from 'isomorphic-unfetch';
+import Error from './_error';
+import { Component } from 'react';
+
+export default class About extends Component {
+	static async getInitialProps() {
+		const res = await fetch('https://api.github.com/users/unnamedph');
+		const statusCode = res.status > 200 ? res.status : false;
+		const data = await res.json();
+
+		return { user: data, statusCode };
+	}
+
+	render() {
+		const { user, statusCode } = this.props;
+
+		if (statusCode) {
+			return <Error statusCode={statusCode} />;
+		}
+
+		return (
+			<Layout title="About">
+				<p>{user.name}</p>
+				<img src={user.avatar_url} alt="unnamedph" height="200px" />
+			</Layout>
+		);
+	}
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
-const Index = () => <div>Welcome to the home page</div>;
+import Layout from "../components/Layout";
+
+const Index = () => (
+  <Layout title="Home">
+    <p>Welcome to the home page</p>
+  </Layout>
+);
 
 export default Index;
