c5c737c4d551685c41b3d41246ca69c581c6fece
Master Ace <gapotanimpuno@gmail.com>
2020-01-11 15:33:51 +0800

    Sec intial

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default = () => (
+    <div>
+        <h1>About
+        </h1>
+        <p> A JavaScript Programmer</p>
+        <img src="/static/javascript-logo.png" alt="JavaScript"></img>
+    </div>
+);
